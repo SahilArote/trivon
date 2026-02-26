2cfe4cc38c62cfae128d9e5e56b5ee06cfbb3236
SahilArote <arotesahil2@gmail.com>
2026-02-26 18:59:53 +0530

    Switched to Brevo professional SMTP for reliable emails

--- trivon/settings.py
+++ trivon/settings.py
@@ -177,13 +177,14 @@ MESSAGE_TAGS = {
 }
 
 
-# SMTP configration
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 465
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp-relay.brevo.com'
+EMAIL_PORT = 587
 EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
-EMAIL_USE_SSL = True  
-# EMAIL_USE_TLS = True
+EMAIL_USE_TLS = True
+
+DEFAULT_FROM_EMAIL = 'arotesahil2@gmail.com'
 
 
 
